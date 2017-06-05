50b18acff5c36b57f75ee748f1ae279c25e64d23
chimmu@126.com <chimmu@126.com>
2017-06-05 10:29:04 +0800

    map debug2

--- cpp_test/stl/map.cpp
+++ cpp_test/stl/map.cpp
@@ -5,27 +5,27 @@
 int main ()
 {
 	std::map<int,int> mymap ;
-        std::map<int,int> mymap2;
-        mymap2[100] = 100;
-        
-        for (int k =0;k< 5;++k) {
-            mymap[k] = k;
-        }
-        
-        for (int k =8;k> 3;--k) {
-            mymap2[k] = k;
-        }
-        
-        mymap2.insert(std::make_pair(10,10));
-	
-        mymap2.insert(mymap.begin(),mymap.end());
+	std::map<int,int> mymap2;
+	mymap2[100] = 100;
 
-        mymap2.erase(1);
-        mymap2.erase(mymap2.begin()+2);
+	for (int k =0;k< 5;++k) {
+		mymap[k] = k;
+	}
+
+	for (int k =8;k> 3;--k) {
+		mymap2[k] = k;
+	}
+
+	mymap2.insert(std::make_pair(10,10));
+
+	mymap2.insert(mymap.begin(),mymap.end());
+
+	mymap2.erase(1);
+	mymap2.erase(mymap2.begin());
 	std::cout << "mymap contains:";
 	for (std::map<int, int>::iterator it=mymap2.begin(); it!=mymap2.end(); ++it)
 	{	
-		std::cout << ' ' << it->second;
+		std::cout << '[' << it->first << ']'<< it->second << std::endl;
 	}
 	std::cout << '\n';
 
