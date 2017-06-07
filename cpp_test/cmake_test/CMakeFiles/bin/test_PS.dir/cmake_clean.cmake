77d706c7b79f7784540ed1f8f76926a145db793b
chimmu@126.com <chimmu@126.com>
2017-06-07 13:37:32 +0800

    add Makefile

---
+++ cpp_test/cmake_test/CMakeFiles/bin/test_PS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin/test_PS.dir/src/test.cpp.o"
+  "bin/test_PS.pdb"
+  "bin/test_PS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin/test_PS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
