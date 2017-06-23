03a322f10fef89113230f61403e3dd35c10c352a
chimmu@126.com <chimmu@126.com>
2017-06-23 18:06:23 +0800

    debug thread-mt

--- my_cmake/src/boost/boost_main.cpp
+++ my_cmake/src/boost/boost_main.cpp
@@ -2,9 +2,6 @@
 #include <boost/thread.hpp> 
 #include <iostream> 
 
-boost::asio::io_service io_service_1;
-	boost::asio::io_service io_service_2;
-	
 void handler1(const boost::system::error_code &ec) {
 	std::cout << "5 seconds." << std::endl;
 }
@@ -12,6 +9,9 @@ void handler1(const boost::system::error_code &ec) {
 void handler2(const boost::system::error_code &ec) {
 	std::cout << "10 seconds." << std::endl;
 }
+
+boost::asio::io_service io_service_1;
+boost::asio::io_service io_service_2;
 
 void thread_func_1() {
 	std::cout << "## first thread ##" << std::endl;
@@ -25,22 +25,20 @@ void thread_func_2() {
 
 int main() {
 	//1-boost:asio time handle
-//	boost::asio::io_service io_service;
-//	boost::asio::deadline_timer timer1(io_service, boost::posix_time::seconds(5));
-//	timer1.async_wait(handler1);
-//	boost::asio::deadline_timer timer2(io_service, boost::posix_time::seconds(10));
-//	timer2.async_wait(handler2);
-//	io_service.run();
-	
+	//	boost::asio::io_service io_service;
+	//	boost::asio::deadline_timer timer1(io_service, boost::posix_time::seconds(5));
+	//	timer1.async_wait(handler1);
+	//	boost::asio::deadline_timer timer2(io_service, boost::posix_time::seconds(10));
+	//	timer2.async_wait(handler2);
+	//	io_service.run();
+
 	//2-boost-asio thread
-	boost::asio::io_service io_service_1;
-	boost::asio::io_service io_service_2;
 	boost::asio::deadline_timer timer1(io_service_1, boost::posix_time::seconds(5));
 	timer1.async_wait(handler1);
-	boost::asio::deadline_timer timer2(io_service_2, boost::posix_time::seconds(5));
+	boost::asio::deadline_timer timer2(io_service_2, boost::posix_time::seconds(4));
 	timer2.async_wait(handler2);
-	boost::thread thread1.run(thread_func_1);
-	boost::thread thread2.run(thread_func_2);
+	boost::thread thread1(thread_func_1);
+	boost::thread thread2(thread_func_2);
 	thread1.join();
 	thread2.join();
 }
